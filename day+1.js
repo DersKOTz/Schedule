fee57f9216d0fcf3135ef71483b4b576486eba50
DersKOTz <98951728+DersKOTz@users.noreply.github.com>
2022-10-30 12:10:00 +0800

    Add files via upload

--- day+1.js
+++ day+1.js
@@ -1,6 +1,6 @@
 Date.prototype.getWeek = function() {
     var onejan = new Date(this.getFullYear(), 0, 1);
-    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() - 8) / 7);
+    return Math.ceil((((this - onejan) / 86400000) + onejan.getDay() - 7) / 7);
 }
 var d = new Date();
  var n = d.getDay()+1;
